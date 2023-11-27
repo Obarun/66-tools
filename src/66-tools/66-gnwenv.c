bff77902d6ef71289e37b8d1c404a821d80028a7
obarun <eric@obarun.org>
2023-11-27 18:50:10 +1100

    update LICENSE date

--- src/66-tools/66-gnwenv.c
+++ src/66-tools/66-gnwenv.c
@@ -1,7 +1,7 @@
 /*
  * 66-gnwenv.c
  *
- * Copyright (c) 2018-2021 Eric Vidal <eric@obarun.org>
+ * Copyright (c) 2018-2023 Eric Vidal <eric@obarun.org>
  *
  * All rights reserved.
  *
--- src/66-tools/execl-cmdline.c
+++ src/66-tools/execl-cmdline.c
@@ -1,7 +1,7 @@
 /*
  * execl-cmdline.c
  *
- * Copyright (c) 2018-2021 Eric Vidal <eric@obarun.org>
+ * Copyright (c) 2018-2023 Eric Vidal <eric@obarun.org>
  *
  * All rights reserved.
  *
